09714854d7536d22be72fe9b3f76a2eb56db95a9
LUCAS RESENDE BRITO <lucasbrito1325@gmail.com>
2026-02-16 23:41:13 -0300

    mudei

--- cod_selectionsort/1000.c
+++ cod_selectionsort/1000.c
@@ -1,36 +1,41 @@
 #include <stdio.h>
+#include <time.h>   
+int comparacoes = 0;
+int movimentacoes= 0;
+
 void SelectionSort(int arr[],int n){
 
 	int j,temp,i,menor;
 	for(i=0;i<n-1;i++){
-	menor=i;
-	printf(" %d passo procurando o menor apartir do numero %d", i+1,i);
-	for(j=0+1;j<n;j++){
-		printf("comparando os valores de %d e %d", arr[menor],arr[i]);
-		if(arr[i]>arr[menor]){
-			menor=j;
-			printf("novo valor minimo %d", arr[menor]);
+		menor=i;
+		printf(" %d passo procurando o menor apartir do numero %d\n", i+1,i);
+		for(j=i+1;j<n;j++){
+			printf("comparando os valores de %d e %d\n", arr[menor],arr[i]);
+			comparacoes++;
+			if(arr[j]<arr[menor]){
+				menor=j;
+				printf("novo valor minimo %d\n", arr[menor]);
+			}
+		}
+		if(menor != i){
+			temp=arr[menor];
+			arr[menor]=arr[i];
+			arr[i]=temp;
+			printf(" os numeros foram trocados\n");
+			movimentacoes++;
+		}
+		printf("Apos a rodada %d os numeros foram trocados\n",i+1);
+		for(int k = 0 ;k<n;k++){
+			printf("%d\n",arr[k]);
 		}
 	}
-	if(menor != i){
-		temp=arr[menor];
-		arr[menor]=arr[i];
-		arr[i]=temp;
-		printf(" os numeros foram trocados");
-	}
-	printf("Apos a rodada %d os numeros foram trocados",i+1);
-	for(int k = 0 ;k<n;k++){
-		printf("%d",arr[k]);
-		printf("\n");
-	}
 }
-}
+
 void MostrarValores(int arr[], int n) {
     for (int i = 0; i < n; i++)
         printf("%d ", arr[i]);
-        printf("\n");
+    printf("\n");
 }
-
 
 int main(){
     int arr[1000];
@@ -39,13 +44,21 @@ int main(){
 	}
     int n = sizeof(arr) / sizeof(arr[0]);
 
-    printf("Começo do array: ");
-    MostrarValores(arr, n);
+    clock_t inicio, fim;
+    double tempo;
+
+    inicio = clock();
 
     SelectionSort(arr, n);
 
-    printf("\nArray apos Selection Sort: ");
-    MostrarValores(arr, n);
+    fim = clock();
+
+    tempo = ((double)(fim - inicio) / CLOCKS_PER_SEC) * 1000;
+
+    printf("\n Tempo de execucao: %.2f ms\n", tempo);
+    printf("\n numero de movimentacoes: %d", movimentacoes);
+    printf("\n numero de comparacoes: %d", comparacoes);
+    
 
     return 0;
 }
